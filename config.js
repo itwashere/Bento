c8a6e97c7c208c85c9e666bb345e0b042c34c052
itwashere <96871275+itwashere@users.noreply.github.com>
2021-12-31 12:31:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   greetingNight: '¡A mimir!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere2518559',
+  weatherKey: '2518559',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
